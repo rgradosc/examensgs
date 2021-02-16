ff8cbaa5c03c668e9017cff397b78a91d318589b
Raúl Grados Carreño <gnr2092@gmail.com>
2021-02-16 09:05:36 -0500

    Removido codigo innecesario
    Se removio codigo innecesario en el query de calculo del año.

--- Application/AddYears/Queries/CalculateYearFuture/CalculateYearFuture.cs
+++ Application/AddYears/Queries/CalculateYearFuture/CalculateYearFuture.cs
@@ -6,19 +6,6 @@ namespace ExamenSGS.Application.AddYears.Queries.CalculateYearFuture
 {
     public class CalculateYearFuture : IQuery<Year>
     {
-        public string CalculateYear(Year futureYear)
-        {
-            var newYear = (futureYear.BirthDate.Year + futureYear.QuantityYears).ToString();
-            var result = string.Empty;
-
-            foreach (char digit in newYear)
-            {
-                result = string.Format("{0}|{1}", result, digit);
-            }
-
-            return result.Substring(1);
-        }
-
         public async Task<string> Calculate(Year futureYear)
         {
             return await Task.Run(() =>
